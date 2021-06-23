84f98a8f2d5080777eb82c3b44785b69956767ef
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-06-22 21:07:20 -0300

    add create tag migration

---
+++ src/shared/infra/database/migrations/1624404282863-CreateTags.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTags1624404282863 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tags',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'NOW()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'NOW()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tags');
+  }
+}
